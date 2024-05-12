a48345ae4ffebbcc7fefc1988b64ffb1c563522c
hubpagec <gitrepos@hubpage.com.br>
2024-05-12 05:34:03 -0300

    test: adding unit tests with pytest

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def test_get_endpoint_response():
+    url = "http://127.0.0.1:4000/users"
+    response = requests.get(url)
+    print(response.content)
+    assert response.status_code == 200
+    assert b"Error retrieving data"
