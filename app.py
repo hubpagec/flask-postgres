d991f366763376ac6fa955a0ab832ae24e7bdecd
hubpagec <gitrepos@hubpage.com.br>
2024-05-12 05:31:42 -0300

    fix: correcting code identation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DB_URL")
 db = SQLAlchemy(app)
