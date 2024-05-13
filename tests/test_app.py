287be3a93f7841a58616a4d2a664db4292033893
hubpagec <gitrepos@hubpage.com.br>
2024-05-12 22:58:12 -0300

    adjusting

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import requests
 
 
 def test_get_endpoint_response():
-    url = "http://127.0.0.1:8080/users"
+    url = "http://127.0.0.1:4000/users"
     response = requests.get(url)
     print(response.content)
     assert response.status_code == 200
